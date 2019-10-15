56d4da6cbaaec0e8c9803d3dcaa7c424db589b7d
Kevin A <12297645+Kevin-Abd@users.noreply.github.com>
2019-10-15 21:40:58 +0330

    Add project files.

---
+++ OneDimentionWaveMassSpringSimulation/Form1.cs
@@ -0,0 +1,13 @@
+﻿namespace OneDimentionWaveMassSpringSimulation
+{
+	using System.Windows.Forms;
+
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+	}
+}
