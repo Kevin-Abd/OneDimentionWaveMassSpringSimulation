d3741aefb7dd8a151de636161c929bedb6d22397
Kevin A <12297645+Kevin-Abd@users.noreply.github.com>
2019-10-15 22:11:39 +0330

    Added todos Added Engine Structure

---
+++ OneDimentionWaveMassSpringSimulation/Engine/Core.cs
@@ -0,0 +1,38 @@
+﻿namespace OneDimentionWaveMassSpringSimulation.Engine
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using MassSpringLibrary;
+
+	public class Core
+	{
+		private Mass[] Masses { get; }
+
+		/// <summary>
+		/// spring[i] represens the spring between mass[i] and mass[i+1]
+		/// </summary>
+		public Spring[] Springs { get; }
+
+		public Core(Mass[] masses, Spring[] springs)
+		{
+			this.Masses = masses ?? throw new ArgumentNullException(nameof(masses));
+			this.Springs = springs ?? throw new ArgumentNullException(nameof(springs));
+		}
+
+		public void Tick(float dt)
+		{
+			foreach (var s in Springs)
+			{
+				s.Update();
+			}
+
+			foreach (var m in Masses)
+			{
+				m.UpdateAcceleration();
+				m.UpdateSpeed(dt);
+				m.UpdatePostion(dt);
+			}
+		}
+	}
+}
---
+++ OneDimentionWaveMassSpringSimulation/Engine/Interface.cs
@@ -0,0 +1,16 @@
+﻿namespace OneDimentionWaveMassSpringSimulation.Engine.Engine
+{
+	public class Interface
+	{
+		public int Size { get; }
+
+
+		// todo CreateUniformRope(int size)
+		// todo CreateRope(float[] massArray)
+		// todo CreateRope(string expression)?
+		// todo SetEnds(bool isLefSoft,bool isRightSoft)
+		// todo AddWave() ????
+		// todo Draw(PrimaryForm form)
+		// todo return Masses for external drawing methods
+	}
+}
